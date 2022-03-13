47d225e112e72627dd5adc741e0607cf8f48ce61
SUNDARAVADIVU R <suganyajap@gmail.com>
2022-03-13 08:07:43 +0530

    done

---
+++ index.js
@@ -0,0 +1,28 @@
+const express=require('express');
+const app=express();
+const mongoose=require('mongoose');
+const dotenv=require('dotenv');
+const userRoute=require('./routes/user');
+const authRoute=require('./routes/auth');
+const productRoute=require('./routes/product');
+const cartRoute=require('./routes/cart');
+const orderRoute=require('./routes/order');
+const stripeRoute=require('./routes/stripe');
+const cors= require("cors");
+dotenv.config();
+
+const PORT=process.env.PORT;
+mongoose
+.connect(process.env.MONGO_URL)
+.then(()=>console.log("DB connected successfully"))
+.catch((err)=>console.log(err))
+
+app.use(cors());
+app.use(express.json());
+app.use("/auth",authRoute);
+app.use("/users",userRoute);
+app.use("/products",productRoute);
+app.use("/carts",cartRoute);
+app.use("/orders",orderRoute);
+app.use("/checkout",stripeRoute);
+app.listen(PORT,()=>console.log("App is started in ",PORT));
